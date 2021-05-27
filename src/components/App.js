a0d67b456db76fd0b574ce849a2ff4cb5f7400b2
Patrick Asu <patrickasu@gmail.com>
2021-05-27 19:46:11 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,208 @@
+import { Tabs, Tab } from 'react-bootstrap'
+import dBank from '../abis/dBank.json'
+import React, { Component } from 'react';
+import Token from '../abis/Token.json'
+import peaLogo from '../logo2.png';
+import Web3 from 'web3';
+import './App.css';
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadBlockchainData(this.props.dispatch)
+  }
+
+  async loadBlockchainData(dispatch) {
+    if(typeof window.ethereum!=='undefined'){
+      const web3 = new Web3(window.ethereum)
+      const netId = await web3.eth.net.getId()
+      const accounts = await web3.eth.getAccounts()
+
+      //load balance
+      if(typeof accounts[0] !=='undefined'){
+        const balance = await web3.eth.getBalance(accounts[0])
+        this.setState({account: accounts[0], balance: balance, web3: web3})
+      } else {
+        window.alert('Please login with MetaMask')
+      }
+
+      //load contracts
+      try {
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address)
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address)
+        const dBankAddress = dBank.networks[netId].address
+        this.setState({token: token, dbank: dbank, dBankAddress: dBankAddress})
+      } catch (e) {
+        console.log('Error', e)
+        window.alert('Contracts not deployed to the current network')
+      }
+
+    } else {
+      window.alert('Please install MetaMask')
+    }
+  }
+
+  async deposit(amount) {
+    if(this.state.dbank!=='undefined'){
+      try{
+        await this.state.dbank.methods.deposit().send({value: amount.toString(), from: this.state.account})
+      } catch (e) {
+        console.log('Error, deposit: ', e)
+      }
+    }
+  }
+
+  async withdraw(e) {
+    e.preventDefault()
+    if(this.state.dbank!=='undefined'){
+      try{
+        await this.state.dbank.methods.withdraw().send({from: this.state.account})
+      } catch(e) {
+        console.log('Error, withdraw: ', e)
+      }
+    }
+  }
+
+  async borrow(amount) {
+    if(this.state.dbank!=='undefined'){
+      try{
+        await this.state.dbank.methods.borrow().send({value: amount.toString(), from: this.state.account})
+      } catch (e) {
+        console.log('Error, borrow: ', e)
+      }
+    }
+  }
+
+  async payOff(e) {
+    e.preventDefault()
+    if(this.state.dbank!=='undefined'){
+      try{
+        const collateralEther = await this.state.dbank.methods.collateralEther(this.state.account).call({from: this.state.account})
+        const tokenBorrowed = collateralEther/2
+        await this.state.token.methods.approve(this.state.dBankAddress, tokenBorrowed.toString()).send({from: this.state.account})
+        await this.state.dbank.methods.payOff().send({from: this.state.account})
+      } catch(e) {
+        console.log('Error, pay off: ', e)
+      }
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      web3: 'undefined',
+      account: 0X0,
+      token: null,
+      dbank: null,
+      balance: 0,
+      dBankAddress: null
+    }
+  }
+
+  render() {
+    return (
+      <div className='text-monospace'>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarMenu">
+                <img src={peaLogo} class="logo" alt="PEA Token Logo" /> 
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+                    {/* <li class="nav-item"><a href="/reservations" class="nav-link">About us</a></li> */}
+                    {/* <li class="nav-item"><a href="/offers" class="nav-link">White Paper</a></li> */}
+                    <li class="nav-item"><a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en" target="blank" class="nav-link">Connect to wallet</a></li>
+                    {/* <li class="nav-item active"><a href="/menu" class="nav-link">ETH: {this.state.balance}</a></li> */}
+                    <li class="nav-item active"><a href="/menu" class="nav-link">{this.state.account}</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div className="container-fluid mt-5 text-center">
+        <br></br>
+          <h1 className="pea-header">Welcome to PEA</h1>
+          <p className="pea-dsc">Earn high yields on stablecoin loans and borrow capital <br/> without collateral</p>
+          <br></br>
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                <Tab eventKey="deposit" title="Deposit">
+                  <div>
+                  <br></br>
+                  <p className="pea-dsc">How much do you want to deposit? <br/> (min. amount is 0.01 ETH)</p>
+                    <form onSubmit={(e) => {
+                      e.preventDefault()
+                      let amount = this.depositAmount.value
+                      amount = amount * 10**18 //convert to Wei
+                      this.deposit(amount)
+                    }}>
+                      <div className='form-group mr-sm-2'>
+                      <br></br>
+                        <input
+                          id='depositAmount'
+                          step="0.01"
+                          type='number'
+                          ref={(input) => { this.depositAmount = input }}
+                          className="form-control form-control-md"
+                          placeholder='amount...'
+                          required />
+                      </div>
+                      <button type='submit' className='btn btn-primary'>DEPOSIT FUND</button>
+                    </form>
+
+                  </div>
+                </Tab>
+                <Tab eventKey="withdraw" title="Withdraw">
+                  <br></br>
+                  <p className="pea-dsc">Do you want to withdraw + take interest?</p>
+                    <br></br>
+                  <div>
+                    <button type='submit' className='btn btn-primary' onClick={(e) => this.withdraw(e)}>WITHDRAW FUND</button>
+                  </div>
+                </Tab>
+                <Tab eventKey="borrow" title="Borrow">
+                  <div>
+
+                  <br></br>
+                  <p className="pea-dsc">(min. amount is 0.01 ETH)Do you want to borrow tokens? <br/> (You'll get 50% of collateral, in Tokens)</p>
+                    <br></br>
+                    <form onSubmit={(e) => {
+
+                      e.preventDefault()
+                      let amount = this.borrowAmount.value
+                      amount = amount * 10 **18 //convert to wei
+                      this.borrow(amount)
+                    }}>
+                      <div className='form-group mr-sm-2'>
+                        <input
+                          id='borrowAmount'
+                          step="0.01"
+                          type='number'
+                          ref={(input) => { this.borrowAmount = input }}
+                          className="form-control form-control-md"
+                          placeholder='amount...'
+                          required />
+                      </div>
+                      <button type='submit' className='btn btn-primary'>BORROW FUND</button>
+                    </form>
+                  </div>
+                </Tab>
+                <Tab eventKey="payOff" title="Payoff">
+                  <div>
+
+                  <br></br>
+                  <p className="pea-dsc">Do you want to payoff the loan? <br/> (You'll receive your collateral - fee)</p>
+                    <br></br>
+                    <button type='submit' className='btn btn-primary' onClick={(e) => this.payOff(e)}>PAYOFF FUND</button>
+                  </div>
+                </Tab>
+              </Tabs>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
